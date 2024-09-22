bad75390cf77154046a9c23ffbc04320d3ad2f4e
luong-hau-hoa <luonghauhoa05@gmail.com>
2024-09-22 17:38:24 +0700

    Buoi3-code-record_cmake

---
+++ record_cmake/build/CMakeFiles/Myproject.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Myproject.dir/main.cpp.o"
+  "CMakeFiles/Myproject.dir/main.cpp.o.d"
+  "Myproject"
+  "Myproject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Myproject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ record_cmake/main.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+
+int main(int argc, char* argv[])
+{
+    std::cout << "hello, dung cmake \n";
+    return 0;
+}
